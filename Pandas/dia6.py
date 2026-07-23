b1df18a233a10104ce2ea88077ba31d8cf4dfadb
MiguelDev <miguel.ang.cv95@gmail.com>
2026-07-22 20:51:04 -0500

    Limpieza, filtrado, agregaciones

--- Pandas/dia6.py
+++ Pandas/dia6.py
@@ -62,3 +62,86 @@ serie_dividida = serie_numerica/10
 
 print(serie_doble)
 print(serie_dividida)
+
+
+data = {
+    'ID': [1, 2, 3, 4, 5],
+    'Producto': ['Producto A', 'Producto B', None, 'Producto D', 'Producto E'],
+    'Cantidad': [10, 20, 30, None, 50],
+    'Precio': [100, 200, 300, 400, None]
+}
+
+df = pd.DataFrame(data)
+
+print(df.isnull().sum())
+
+
+# import pandas as pd
+
+datos = {
+    'ID': [1, 2, 3, 4, 1],
+    'Producto': ['Producto A', 'Producto B', 'Producto C', 'Producto D', 'Producto A'],
+    'Cantidad': [10, 20, 30, 40, 50],
+    'Precio': [100, 200, 300, 400, 100]
+}
+
+df_sin_duplicados = pd.DataFrame(datos).drop_duplicates(subset='ID')
+
+print(df_sin_duplicados)
+
+
+datax = {
+    'ID': [1, 2, 3, 4],
+    'Producto': ['Producto A', 'Producto B', 'Producto C', 'Producto D'],
+    'Cantidad': [10, 20, 30, 40],
+    'Precio': [100, None, 300, None]
+}
+
+df = pd.DataFrame(datax)
+valores_nuevos = { 'Precio' : df['Precio'].mean() }
+df = df.fillna(valores_nuevos)
+
+print(df)
+
+
+# Filtrado de Series
+
+valores = pd.Series([18, 22, 7, 9, 15, 8])
+condicion_valores_pares = valores % 2 == 0
+print(valores[condicion_valores_pares])
+
+
+frutas = pd.Series(["manzana", "banana", "cereza", "durazno", "frambuesa"])
+frutas_con_e = frutas.str.contains('e')
+print(frutas[frutas_con_e])
+
+
+# Agregaciones
+
+edades = [23, 30, 26, 27, 22, 24, 25, 28]
+edades_df = pd.Series(edades)
+promedio_edades = edades_df.mean()
+print(promedio_edades)
+
+
+ventas = [120, 150, 90, 200, 210, 130, 160]
+serie = pd.Series(ventas,['Lunes','Martes','Miércoles','Jueves','Viernes','Sábado','Domingo'])
+suma_total_ventas = serie.sum()
+dia_mayores_ventas = serie.idxmax()
+promedio_ventas = serie.mean()
+
+print(suma_total_ventas)
+print(dia_mayores_ventas)
+print(promedio_ventas)
+
+
+ventas_mes = pd.Series([220, 235, 260, 213, 202, 298, 265, 198, 220, 230, 190, 215, 275, 222, 218, 245, 233, 210, 290, 210,
+                        215, 220, 225, 230, 245, 250, 260, 270, 280, 295])
+                        
+total_ventas_mes = ventas_mes.sum()
+dia_ventas_mas_bajas = ventas_mes.min()
+promedio_ventas_mes = ventas_mes.mean()
+
+print(total_ventas_mes)
+print(dia_ventas_mas_bajas)
+print(promedio_ventas_mes)
